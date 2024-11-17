78039b7c02b450f601003efcfade0e0687c212b1
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-11-17 01:38:56 +0100

    Started cpu test

--- src/tests/FancyMagicTest.cu
+++ src/tests/FancyMagicTest.cu
@@ -21,23 +21,11 @@
 
 __device__ static constexpr unsigned TEST_SIZE = 1'000'000;
 
-__device__ __uint64_t rotl64(const __uint64_t x, int k) {
-    return (x << k) | (x >> (64 - k));
-}
-
-__device__ __uint64_t xoshiro256starstar(uint64_t& s0, uint64_t& s1, uint64_t& s2, uint64_t& s3) {
-    const __uint64_t result = rotl64(s1 * 5, 7) * 9;
-    const __uint64_t t = s1 << 17;
-
-    s2 ^= s0;
-    s3 ^= s1;
-    s1 ^= s2;
-    s0 ^= s3;
-
-    s2 ^= t;
-    s3 = rotl64(s3, 45);
-
-    return result;
+__device__ __forceinline__ __uint64_t simpleRand(__uint64_t &state) {
+    state ^= state << 13;
+    state ^= state >> 7;
+    state ^= state << 17;
+    return state;
 }
 
 template<typename MapT>
@@ -45,18 +33,16 @@ __global__ void FancyMagicKernel(const __uint64_t *seeds, __uint64_t *results) {
     const unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
     const __uint64_t seed = seeds[idx];
 
-    __uint64_t s0 = seed;
-    __uint64_t s1 = s0 + 0x9E3779B97F4A7C15ULL;
-    __uint64_t s2 = s1 + 0x9E3779B97F4A7C15ULL;
-    __uint64_t s3 = s2 + 0x9E3779B97F4A7C15ULL;
-
     unsigned randomPos = idx % 64;
+    __uint64_t board = seed;
 
     for (unsigned i = 0; i < TEST_SIZE; ++i) {
-        const __uint64_t randomBoard = xoshiro256starstar(s0, s1, s2, s3);
-        randomPos = (randomPos + (randomBoard & 63)) % 64;
+        board = simpleRand(board);
+        randomPos = (randomPos + (board & 63)) & 64;
 
-        ++results[idx];
+        const auto result = MapT::GetMoves(randomPos, board);
+
+        results[idx] += result;
     }
 }
 
@@ -129,8 +115,8 @@ void FancyMagicTest_(int threadsAvailable, const cudaDeviceProp &deviceProps) {
 
 void FancyMagicTest(int threadsAvailable, const cudaDeviceProp &deviceProps) {
     try {
-//        FancyMagicTest_(threadsAvailable, deviceProps);
-        cpu::FancyMagicTest();
+//        cpu::FancyMagicTest();
+        FancyMagicTest_(threadsAvailable, deviceProps);
     } catch (const std::exception &e) {
         std::cerr << "Fancy Magic Test failed with exception: " << e.what() << std::endl;
     }
--- src/tests/cpu/FancyTest.cpp
+++ src/tests/cpu/FancyTest.cpp
@@ -5,11 +5,59 @@
 #include "CpuTests.h"
 
 #include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <format>
+
+#include "../../../engine/include/MoveGeneration/BishopMap.h"
+
+static constexpr unsigned TEST_SIZE = 1'000'000;
+
+uint64_t simpleRand(uint64_t &state) {
+    state ^= state << 13;
+    state ^= state >> 7;
+    state ^= state << 17;
+    return state;
+}
 
 namespace cpu {
 
     void FancyMagicTest() {
-        std::cout << "Fancy magic test ON TGHE GHK CPU!" << std::endl;
+        std::cout << "Running fancy magic test on the CPU!" << std::endl;
+
+        std::mt19937_64 rng(std::random_device{}());
+        const unsigned seed = rng();
+        const uint64_t idx = 0;
+
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        unsigned randomPos = idx;
+        uint64_t board = seed;
+        uint64_t control = 0;
+        for (unsigned i = 0; i < TEST_SIZE; ++i) {
+            board = simpleRand(board);
+            randomPos = (randomPos + (board & 63)) & 64;
+
+            const uint64_t moves = BishopMap::GetMoves(static_cast<int>(randomPos), board);
+            control += moves;
+        }
+
+        auto t2 = std::chrono::high_resolution_clock::now();
+        const unsigned threadsUtilized = std::thread::hardware_concurrency();
+
+        const double seconds = std::chrono::duration<double>(t2 - t1).count();
+        const double milliseconds = seconds * 1000.0;
+        const double readsPerSecond = (TEST_SIZE * threadsUtilized) / seconds;
+        const double readsPerMillisecond = (TEST_SIZE * threadsUtilized) / milliseconds;
+
+        std::cout << std::format("Fancy Magic Test for {} took {} seconds, {} milliseconds,\n"
+                                 "Average performance was:\n"
+                                 "reads per second: {}\n"
+                                 "reads per millisecond: {}",
+                                 "BISHOPS", seconds, milliseconds, readsPerSecond, readsPerMillisecond) << std::endl;
+
+        std::cout << "Control sum of the results: " << control << std::endl;
     }
 
 } // namespace cpu
