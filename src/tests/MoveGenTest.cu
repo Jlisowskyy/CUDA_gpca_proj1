737150218333cd7813fa0337fb09fd8ed404910a
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-11-20 18:42:04 +0100

    Passing move gen test

--- src/tests/MoveGenTest.cu
+++ src/tests/MoveGenTest.cu
@@ -24,16 +24,16 @@
 using u16d = std::bitset<16>;
 
 static constexpr std::string_view TestFEN[]{
-//        "3r2k1/B7/4q3/1R4P1/1P3r2/8/2P2P1p/R4K2 w - - 0 49",
-//        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-//        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-//        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
-//        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-//        "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+        "3r2k1/B7/4q3/1R4P1/1P3r2/8/2P2P1p/R4K2 w - - 0 49",
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
         "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-//        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
-//        "3rr1k1/1pq2pp1/p2nb2p/2pp4/6PR/2PBPN2/PPQ2PP1/K2R4 b - - 0 20",
-//        "7k/r2q1ppp/1p1p4/p1bPrPPb/P1PNPR1P/1PQ5/2B5/R5K1 w - - 23 16",
+        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        "3rr1k1/1pq2pp1/p2nb2p/2pp4/6PR/2PBPN2/PPQ2PP1/K2R4 b - - 0 20",
+        "7k/r2q1ppp/1p1p4/p1bPrPPb/P1PNPR1P/1PQ5/2B5/R5K1 w - - 23 16",
 };
 
 void __global__ GetGPUMovesKernel(const cuda_Board *board, cuda_Move *outMoves, int *outCount) {
@@ -66,14 +66,7 @@ void TestSinglePositionOutput(const std::string_view &fen) {
         throw std::runtime_error("Failed to launch kernel");
     }
 
-    cpu::external_board eBoard{};
-    for (size_t i = 0; i < BitBoardsCount; ++i)
-        eBoard[i] = board.BitBoards[i];
-    eBoard[12] = board.ElPassantField;
-    eBoard[13] = board.Castlings;
-    eBoard[14] = board.MovingColor;
-
-    const auto cMoves = cpu::GenerateMoves(eBoard);
+    const auto cMoves = cpu::GenerateMoves(externalBoard);
 
     thrust::host_vector<cuda_Move> hMoves = dMoves;
     thrust::host_vector<int> hCount = dCount;
